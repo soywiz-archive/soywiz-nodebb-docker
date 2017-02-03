b781504a005549c64e675882641a63c4175a10ca
Carlos Ballesteros Velasco <soywiz@gmail.com>
2017-02-03 01:24:31 +0100

    Small fix

--- nodeapp.sh
+++ nodeapp.sh
@@ -1,9 +1,9 @@
 #!/usr/bin/env bash
 
-if [[ ! -d /app ]]; then
-    echo "=> An empty or uninitialized /app volume is detected"
-    echo "=> Installing MySQL ..."
-    cp -rf /app-base /app
+if [ ! -f /app/app.js ]; then
+    echo "=> Can't find /app/app.js copying from /app-base"
+    mkdir -p /app
+    cp -rf /app-base/* /app
     echo "=> Done!"  
 else
     echo "=> Using an existing volume /app"
