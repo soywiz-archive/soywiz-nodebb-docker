14652fb69c3aaecb378965a9bd46ce200c43974e
Carlos Ballesteros Velasco <soywiz@gmail.com>
2017-02-03 01:10:46 +0100

    Export port

--- run.sh
+++ run.sh
@@ -2,6 +2,7 @@
 export DOMAIN=localhost
 BASENAME=$(dirname "$(readlink -f "$0")")
 docker run -d --name $DOMAIN \
+  -p 80:4567 \
   -l host=$DOMAIN \
   -v $BASENAME/app:/app \
   -v $BASENAME/mongodb:/var/lib/mongodb \
